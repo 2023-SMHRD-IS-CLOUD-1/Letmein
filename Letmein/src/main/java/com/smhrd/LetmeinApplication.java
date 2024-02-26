d9a9fa6300af87113627639e3990bae418b273b7
yusuyeon1111 <yusuyeon443@gmail.com>
2024-02-26 10:03:54 +0900

    commit

--- Letmein/src/main/java/com/smhrd/LetmeinApplication.java
+++ Letmein/src/main/java/com/smhrd/LetmeinApplication.java
@@ -2,10 +2,17 @@ package com.smhrd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LetmeinApplication {
+public class LetmeinApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LetmeinApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LetmeinApplication.class, args);
 	}
